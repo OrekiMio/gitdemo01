6f17204d062f8de428faf4013ede1c113370ce19
hwt <1964959721@qq.com>
2022-09-09 13:44:22 +0800

    sshpush

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello hot-fix-conflict");
         System.out.println("master-fix");
         System.out.println("hot-fix");
+        System.out.println("sshpush");
     }
 }
